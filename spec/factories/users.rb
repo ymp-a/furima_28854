b7662cbba0d8b79900dee1d8c240d094e6cf0170
ymp-a <hiyakashi3@gmail.com>
2020-08-25 21:46:22 +0900

    Factoryファイルへusersテストデータの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 1)}
+    nickname              {Faker::Name.last_name}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name             {"田中"}
+    first_name            {"太郎"}
+    kana_last_name        {"タナカ"}
+    kana_first_name       {"タロウ"}
+    birthdate             {2020/02/02}
   end
 end
