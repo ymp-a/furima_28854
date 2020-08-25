d69dd023c5f2a1a968801325650ad7c4e24fb3ab
ymp-a <hiyakashi3@gmail.com>
2020-08-25 17:10:04 +0900

    エラーメッセージを英語で出力する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
+    nickname              {Faker::Name.initials(number: 1)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
