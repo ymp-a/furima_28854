3cfe0db4a5c509a4e4597e751c4d9d95cc2d3c7b
ymp-a <hiyakashi3@gmail.com>
2020-09-01 14:54:51 +0900

    itemsコントローラーのストロングパロメーター記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :item_descrption, :category_id, :status_id, :cost_bearer_id, :consignor_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
 end
