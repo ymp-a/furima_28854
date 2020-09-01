02a46e5b8950de0a1256badfeb3258875b533a86
ymp-a <hiyakashi3@gmail.com>
2020-09-01 14:56:57 +0900

    active_hash,modelのvalidates,viewファイルへの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,36 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :cost_bearer
+  belongs_to_active_hash :consignor
+  belongs_to_active_hash :delivery_date
+  
   belongs_to :user
   # has_one :deal
   has_one_attached :image
 
-  
+
+  #空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :name
+    validates :item_descrption
+    validates :category
+    validates :status
+    validates :cost_bearer
+    validates :consignor
+    validates :delivery_date
+    validates :price
+  end
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :cost_bearer_id
+    validates :consignor_id
+    validates :delivery_date_id  
+  end
+
+
 end
