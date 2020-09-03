47ddbb61c61f42a95d6b29733fb14fae34aba40e
ymp-a <hiyakashi3@gmail.com>
2020-09-03 11:08:51 +0900

    未ログインユーザーをログイン画面へ遷移させる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,14 @@
 class ItemsController < ApplicationController
+
   def index
     @item = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 
   def create
