61ef158e49e115ea5bdbcd8c40fdc74f51259ba2
ymp-a <hiyakashi3@gmail.com>
2020-09-10 11:01:59 +0900

    dealsテーブルルーティング記述（ネスト）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root  "items#index"
-  resources :items
+  resources :items do
+    resources :deals, only: [:index, :create]
+  end
   get 'items/:price', to: 'items#new'
 
 end
